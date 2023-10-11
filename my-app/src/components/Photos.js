e367aed4991495710e1bb63db43f2d7e40ccec5c
AdamJednacz <adamjednacz4@gmail.com>
2023-10-11 17:32:44 +0200

    galeria

--- my-app/src/components/Photos.js
+++ my-app/src/components/Photos.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import img5 from "../assets/PEFC - logo.png";
+import ImageModal from "./ImageModal";
 
 const Photos = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const openModal = (imageSrc) => {
+        setSelectedImage(imageSrc);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+        setIsModalOpen(false);
+    };
+
     return (
-        <section className="price" id="photos">
+        <section className="photos" id="photos">
             <div className="container">
             <h1>Galeria</h1>
+             <div className="photos_items">
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+             </div>
+             <div className="photos_items">
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+                 <img src={img5} alt={img5} className="photos_item" onClick={() => openModal(img5)}></img>
+             </div>
             </div>
+            <ImageModal isOpen={isModalOpen} closeModal={closeModal} imageSrc={selectedImage} />
         </section>
     );
 };
