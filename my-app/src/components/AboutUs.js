f9eff7ae501dc317aca0d98974b0bd88902431cd
AdamJednacz <adamjednacz4@gmail.com>
2023-12-26 18:19:17 +0100

    lazyloader

--- my-app/src/components/AboutUs.js
+++ my-app/src/components/AboutUs.js
@@ -11,13 +11,13 @@ const AboutUs = () => {
             <div className="container">
                 <div className="about_us_items">
                 <img className="img1" src={img1} alt={img1}/>
-                 <div className="about_us_text_item">
+                <div className="about_us_text_item">
                 <h1>O nas</h1>
                 <p>Oto nasza nowa firma, która powstała z pasji do klimatyzacji i zaangażowania w dostarczanie najlepszych rozwiązań. Jesteśmy ekspertami w dziedzinie montażu, sprzedaży i serwisu urządzeń klimatyzacyjnych. Naszym celem jest zapewnienie Ci idealnego klimatu w Twoim otoczeniu, bez względu na porę roku.</p>
                      <p>Jeżeli masz już jakieś pytania to:</p>
 
                      <Link to="contact" smooth={true} duration={500}><button>Zadzwoń</button></Link>
-                 </div>
+                </div>
                 </div>
             </div>
         </section>
--- my-app/src/components/Offer.js
+++ my-app/src/components/Offer.js
@@ -2,27 +2,60 @@ import React from 'react';
 import img2 from "../assets/wykalifikowany.jpg"
 import img3 from "../assets/zblizenie-pracownika-fizycznego-w-zakladzie-przemyslowej-linii-produkcyjnej.jpg"
 import img4 from "../assets/brodaty-mezczyzna-w-ogolnej-uzyciu-tabletu.jpg"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
  const Offer = () =>{
-    return (
+     const offerItems = [
+         {
+             title: "Montaż",
+             image: {
+                 alt: "Montaż Image Alt",
+                 src: img2,
+                 height: img2.height,
+                 width: img2.width
+             },
+             description: "Zaufaj naszemu doświadczeniu i ekspertyzie. Oferujemy kompleksowy montaż klimatyzacji, zapewniając optymalną wydajność i niezawodność."
+         },
+         {
+             title: "Serwis",
+             image: {
+                 alt: "Serwis Image Alt",
+                 src: img3,
+                 height: img3.height,
+                 width: img3.width
+             },
+             description: "Nasi wykwalifikowani technicy stoją są do Twojej dyspozycji. Gwarantujemy fachowy i szybki serwis klimatyzacji, aby utrzymać ją w doskonałej formie. Twój komfort jest naszym priorytetem."
+         },
+         {
+             title: "Sprzedaż",
+             image: {
+                 alt: "Sprzedaż Image Alt",
+                 src: img4,
+                 height: img4.height,
+                 width: img4.width
+             },
+             description: "Oferujemy najnowocześniejsze urządzenia klimatyzacyjne od renomowanych producentów. Nasz zespół doradców pomoże Ci znaleźć idealny model, spełniający Twoje potrzeby. Daj nam szansę dostarczenia Ci komfortu przez cały rok."
+         }
+     ];
+
+     return (
         <section className="offer" id="offer">
             <div className="container">
                 <h1>Oferta</h1>
                 <div className="offer_items">
-                    <div className="offer_item">
-                        <h2>Montaż</h2>
-                        <img src={img2} alt={img2}/>
-                        <p>Zaufaj naszemu doświadczeniu i ekspertyzie. Oferujemy kompleksowy montaż klimatyzacji, zapewniając optymalną wydajność i niezawodność. Daj nam zadanie, a stworzymy idealne warunki klimatyczne w Twoim otoczeniu.</p>
-                    </div>
-                    <div className="offer_item">
-                        <h2>Serwis</h2>
-                        <img src={img3} alt={img3}/>
-                        <p>Nasi wykwalifikowani technicy stoją są do Twojej dyspozycji. Gwarantujemy fachowy i szybki serwis klimatyzacji, aby utrzymać ją w doskonałej formie. Twój komfort jest naszym priorytetem.</p>
-                    </div>
-                    <div className="offer_item">
-                        <h2>Sprzedaż</h2>
-                        <img src={img4} alt={img4}/>
-                        <p>Oferujemy najnowocześniejsze urządzenia klimatyzacyjne od renomowanych producentów. Nasz zespół doradców pomoże Ci znaleźć idealny model, spełniający Twoje potrzeby. Daj nam szansę dostarczenia Ci komfortu przez cały rok.</p>
-                    </div>
+                    {offerItems.map((item, index) => (
+                        <div className="offer_item" key={index}>
+                            <h2>{item.title}</h2>
+                            <LazyLoadImage
+                                alt={item.image.alt}
+                                src={item.image.src}
+                                height={item.image.height}
+                                width={item.image.width}
+                                effect="blur"
+                            />
+                            <p>{item.description}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
