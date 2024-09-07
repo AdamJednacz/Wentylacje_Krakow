2be44fa8d84bfddaaf37bc7023b989c06a74793e
AdamJednacz <adamjednacz4@gmail.com>
2024-09-07 18:52:16 +0200

    o nas

--- my-app/src/components/AboutUs.js
+++ my-app/src/components/AboutUs.js
@@ -3,19 +3,17 @@ import img1 from "./assets/about_us.webp";
 import {Link} from "react-scroll";
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import logo from "./assets/frozekologo-shading.svg";
+
 
 const AboutUs = () => {
     return (
         <section id="aboutus" className="about_us" >
             <div className="container">
-                <LazyLoadImage
+                <img
                     className="about_us_img"
-                    effect="blur"
+
                     src={img1}
                     alt={img1}
-
-
                 />
                 <div className="about_us_text_item">
                     <h1>O nas</h1>
